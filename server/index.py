30b51b8f06f6e2453ae7ead91400c9a315ba0bf5
Marlon Ruiz <mjavierrt14@gmail.com>
2023-07-15 08:15:42 -0500

    updated mode server

--- server/index.py
+++ server/index.py
@@ -1,6 +1,6 @@
 from app import app
-from config import  DevelopmentConfig
+from config import config
 
 if __name__ == '__main__':
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(config['mode'])
     app.run(port=app.config['PORT'], host=app.config['HOST'])
